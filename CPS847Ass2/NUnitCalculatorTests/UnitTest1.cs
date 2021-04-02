e544e0c4577d504afccd6f12f6ae8626abe32abd
michaelGribun <michael.gribun@ryerson.ca>
2021-04-02 14:44:31 -0400

    lots of files to hopefully get travis to finally work
    pls work

--- CPS847Ass2/NUnitCalculatorTests/UnitTest1.cs
+++ CPS847Ass2/NUnitCalculatorTests/UnitTest1.cs
@@ -1,31 +1,34 @@
-﻿using CPS847Ass2;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using NUnit.Framework;
+using CPS847Ass2;
 
-namespace CalculatorTests
+namespace NUnitCalculatorTests
 {
-    [TestClass]
-    public class CalculatorTests
+    public class Tests
     {
-        [TestMethod]
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
         public void addTest()
         {
             Assert.AreEqual(Calculator.addThem(1.0, 2.0), 3.0);
         }
 
-        [TestMethod]
+        [Test]
         public void subTest()
         {
             Assert.AreEqual(Calculator.subtractThem(4.0, 2.0), 2.0);
         }
 
-        [TestMethod]
+        [Test]
         public void multTest()
         {
             Assert.AreEqual(Calculator.multiplyThem(2.0, 2.0), 4.0);
         }
 
-        [TestMethod]
+        [Test]
         public void divTest()
         {
             Assert.AreEqual(Calculator.divideThem(20.0, 2.0), 10.0);
