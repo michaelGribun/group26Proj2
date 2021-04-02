3bb8bfae119eb64bf3183d98edbc31dd93f6a637
michaelGribun <michael.gribun@ryerson.ca>
2021-04-02 16:00:18 -0400

    a lot of updates and changes

---
+++ CPS847Ass2/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using CPS847Ass2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void addTest()
+        {
+            Assert.AreEqual(Calculator.addThem(1.0, 2.0), 3.0);
+        }
+
+        [TestMethod]
+        public void subTest()
+        {
+            Assert.AreEqual(Calculator.subtractThem(4.0, 2.0), 2.0);
+        }
+
+        [TestMethod]
+        public void multTest()
+        {
+            Assert.AreEqual(Calculator.multiplyThem(2.0, 2.0), 4.0);
+        }
+
+        [TestMethod]
+        public void divTest()
+        {
+            Assert.AreEqual(Calculator.divideThem(20.0, 2.0), 10.0);
+        }
+    }
+}
